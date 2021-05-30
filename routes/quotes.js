8dcef547c20823ad89943a1bee62f41f9bbf2a2e
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-05-30 15:37:54 +1000

    Search by Author

--- routes/quotes.js
+++ routes/quotes.js
@@ -24,10 +24,17 @@ router.get('/', async function(req, res, next) {
   }
 });
 
-/* GET quotes listing. */
-router.get('/search', async function(req, res, next) {
+
+/* GET quotes listing search. */
+router.get('/author/:author', async function(req, res, next) {
   try {
-    res.json(await quotes.search(req.query.page, req.query.quote));
+    const page = req.query.page || 1;
+    const author = req.params.author;
+    if (!author) {
+      return res.status(400).json({message: 'please provide author'});
+    }
+    
+    res.json(await quotes.getByAuthor(page, author));
   } catch (err) {
     console.error(`Error while getting quotes `, err.message);
     res.status(err.statusCode || 500).json({'message': err.message});
