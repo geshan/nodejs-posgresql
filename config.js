e98a5b2a1d09edfee16e8accf77380b5b1b68190
James Strachan <james.strachan@gmail.com>
2021-06-23 16:03:46 +0100

    chore: enable ssl

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ const config = {
     user: env.DB_USER || 'cklijfef',
     password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
     database: env.DB_NAME || 'cklijfef',
+    ssl: true
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
