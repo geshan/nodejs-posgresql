3cf66d2f8b70cd080a14c95ad507daab95866bf1
James Strachan <james.strachan@gmail.com>
2021-06-23 17:26:50 +0100

    chore: add new data

--- charts/nodejs-posgresql/init.sql
+++ charts/nodejs-posgresql/init.sql
@@ -23,5 +23,5 @@ INSERT INTO quote (id, quote, author) VALUES
 (14, 'There are only two hard things in Computer Science: cache invalidation and naming things.', 'Phil Karlton'), 
 (15, 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight.', 'Bill Gates'), 
 (16, 'Controlling complexity is the essence of computer programming.', 'Brian Kernighan'),
-(17, 'The only way to learn a new programming language is by writing programs in it.', 'Dennis Ritchie');
+(17, 'The only way to learn a new programming language is by writing programs in it.', 'Dennis Ritchie'),
 (18, 'Jenkins X is totally awesome.', 'James Strachan');
