c0e93c6d8aae58052408bef0ad3414834cf13555
Geshan Manandhar <geshan@gmail.com>
2021-08-30 21:50:36 +1000

    Try out cockroach db

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'otto.db.elephantsql.com',
-    port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'cklijfef',
-    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-    database: env.DB_NAME || 'cklijfef',
+    host: env.DB_HOST || 'free-tier.gcp-us-central1.cockroachlabs.cloud',
+    port: env.DB_PORT || '26257',
+    user: env.DB_USER || 'quotes',
+    password: env.DB_PASSWORD || 'n5sk@pvjT@HcbKU',
+    database: env.DB_NAME || 'cockroach-2039.defaultdb',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
