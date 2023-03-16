eb5b60e3267536c6daab8b4841fec102cf70f9a4
jpercy84 <70901436+jpercy84@users.noreply.github.com>
2023-03-16 17:35:15 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({message: 'alive'});
+  res.json({message: 'alivecc'});
 });
 
 module.exports = router;
