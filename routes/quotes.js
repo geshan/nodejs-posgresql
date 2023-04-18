63a67e675381dc87304240a6a11f449bcb781e1c
Geshan Manandhar <geshan@simplywallst.com>
2023-04-18 21:28:40 +1000

    Add an error delibrately

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const quotes = require('../services/quotes');
+const appSignal = require('@appsignal/nodejs');
+
 
 /* GET quotes listing. */
 router.get('/', async function(req, res, next) {
@@ -21,5 +23,17 @@ router.post('/', async function(req, res, next) {
     res.status(err.statusCode || 500).json({'message': err.message});
   }
 });
+
+/* GET quotes listing error. */
+router.get('/error-fix', async function(req, res, next) {
+  try {
+    res.json(await quotes.getError(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    appSignal.setError(err);
+    res.status(err.statusCode || 500).json({'message': err.message});
+  }
+});
+
 
 module.exports = router;
