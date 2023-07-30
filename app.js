d606e6caa365616912722b7518be37ce58420d43
nikonaime <115784811+nikonaime@users.noreply.github.com>
2023-07-31 01:55:42 +0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var quotesRouter = require('./routes/quotes');
+var paymentRoutes = require('./routes/payment');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
+app.use('/payment', paymentRoutes);
 
 module.exports = app;
